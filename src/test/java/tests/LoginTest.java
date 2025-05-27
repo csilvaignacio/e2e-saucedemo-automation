5f9c867d4c5c487aa88fa94186065f310f5586b6
Alvaro <csilvaignacio@gmail.com>
2025-05-27 12:02:12 -0400

    Agrega caso de prueba para login exitoso

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,15 @@
 package tests;
 
+import jdk.jfr.Description;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
 public class LoginTest {
+    private LoginPage loginPage = new LoginPage();
+
+    @Test
+    @Description("Verificando pagina")
+    public void case01(){
+        loginPage.verifyPage();
+    }
 }
